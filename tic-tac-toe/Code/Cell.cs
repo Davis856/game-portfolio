8897f2513d91451ce7d894b443ae43959262ddb6
Davis856 <57221070+Davis856@users.noreply.github.com>
2021-10-13 20:26:50 +0300

    Tic-Tac-Toe base game with simple functionalities, more UI to be added and gameplay changes

---
+++ tic-tac-toe/Code/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    // Cell properties
+    public Text mLabel;
+    public Button mButton;
+    public Main mMain;
+
+    public void Fill()
+    {
+        mButton.interactable = false;
+
+        mLabel.text = mMain.GetTurnCharacter();
+
+        mMain.Switch();
+    }
+}
---
+++ tic-tac-toe/Code/GameBoard.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    public GameObject mCellPrefab;
+    public GameObject mXLine;
+    public GameObject mYLine;
+    public GameObject mHorLine;
+
+    private Cell[] mCells = new Cell[9];
+
+    // We create cells in the grid
+    public void Build(Main main)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            GameObject newCell = Instantiate(mCellPrefab, transform);
+
+            mCells[i] = newCell.GetComponent<Cell>();
+            mCells[i].mMain = main;
+        }
+    }
+
+    public bool CheckForWinner()
+    {
+        int i = 0;
+        // Horizontal check
+        for (i = 0; i <= 6; i += 3)
+        {
+            if (CheckValues(i, i + 1, i + 2))
+                return true;
+        }
+        // Vertical check
+        for (i = 0; i <= 2; i++)
+        {
+            if (CheckValues(i, i + 3, i + 6))
+                return true;
+        }
+
+        // left Diagonal check
+        if (CheckValues(0, 4, 8))
+            return true;
+
+        // right Diagonal check
+        if (CheckValues(2, 4, 6))
+            return true;
+
+        return false;
+    }
+
+    private bool CheckValues(int firstIndex, int secondIndex, int thirdIndex)
+    {
+        string firstValue = mCells[firstIndex].mLabel.text;
+        string secondValue = mCells[secondIndex].mLabel.text;
+        string thirdValue = mCells[thirdIndex].mLabel.text;
+
+        if (firstValue == "" || secondValue == "" || thirdValue =="")
+            return false;
+
+        if (firstValue == secondValue && firstValue == thirdValue)
+            return true;
+        else
+            return false;
+    }
+
+    public void ResetBoard()
+    {
+        foreach(Cell cell in mCells)
+        {
+            cell.mLabel.text = "";
+            cell.mButton.interactable = true;
+        }
+    }
+
+}
